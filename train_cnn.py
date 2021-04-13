2771c775e6ce5e5481d022c655b59c8d757e8451
joeytxy <66881214+joeytxy@users.noreply.github.com>
2021-04-13 16:47:06 +0800

    Update train_cnn.py

--- train_cnn.py
+++ train_cnn.py
@@ -1,6 +1,11 @@
 import cv2,os
 import numpy as np
 from keras.utils import np_utils
+from matplotlib import pyplot as plt
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Conv2D, Flatten, Dense, Activation,MaxPooling2D, Dropout
+from keras.callbacks import ModelCheckpoint
+from sklearn.model_selection import train_test_split
 
 data=[]
 target=[]
@@ -34,9 +39,6 @@ data=np.reshape(data,(data.shape[0],img_size,img_size,1))
 target=np.array(target)
 target=np_utils.to_categorical(target)
 
-from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Conv2D, Flatten, Dense, Activation,MaxPooling2D, Dropout
-from keras.callbacks import ModelCheckpoint
 
 model=Sequential()
 
@@ -52,26 +54,14 @@ model.add(Dense(50,activation='relu'))
 model.add(Dense(2,activation='softmax'))
 model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
 
-from sklearn.model_selection import train_test_split
-
 train_data,test_data,train_target,test_target=train_test_split(data,target,test_size=0.1)
 checkpoint = ModelCheckpoint('model-{epoch:03d}.model',monitor='val_loss',verbose=0,save_best_only=True,mode='auto')
 history=model.fit(train_data,train_target,epochs=20,callbacks=[checkpoint],validation_split=0.2)
 
-#from matplotlib import pyplot as plt
+plt.plot(history.history['accuracy'],'r',label='training accuracy')
+plt.plot(history.history['val_accuracy'],label='validation accuracy')
+plt.xlabel('# epochs')
+plt.ylabel('loss')
+plt.legend()
+plt.show()
 
-#plt.plot(history.history['loss'],'r',label='training loss')
-#plt.plot(history.history['val_loss'],label='validation loss')
-#plt.xlabel('# epochs')
-#plt.ylabel('loss')
-#plt.legend()
-#plt.show()
-
-#plt.plot(history.history['accuracy'],'r',label='training accuracy')
-#plt.plot(history.history['val_accuracy'],label='validation accuracy')
-#plt.xlabel('# epochs')
-#plt.ylabel('loss')
-#plt.legend()
-#plt.show()
-
-#print(model.evaluate(test_data,test_target))
